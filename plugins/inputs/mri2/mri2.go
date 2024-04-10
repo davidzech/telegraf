ee5317c9655a095b1c3b7bc4e1cb57878dda4b35
david <david@davidzech.com>
2024-04-09 21:52:07 -0700

    Implement MRI V2

---
+++ plugins/inputs/mri2/mri2.go
@@ -0,0 +1,240 @@
+package mri
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/jlaffaye/ftp"
+	"golang.org/x/sync/errgroup"
+)
+
+type Mri2 struct {
+	CollectorName   string
+	Machines        []Machine
+	DefaultUsername string
+	DefaultPassword string
+}
+
+type Machine struct {
+	Name string
+	URL  string
+}
+
+func (m *Mri2) SampleConfig() string {
+	return `
+## Config Vars go below, uncomment '##'
+##
+## CollectorName = "Collector1"
+## Machines = [
+##	{ Name = "Server1", URL = "ftp://server1.com" }, 
+##	{ Name = "Server2", URL = "ftp://overrideUser:overridePassword@server2.com" }
+## ]
+## DefaultUsername = "user"
+## DefaultPassword = "daki123"
+`
+}
+
+func (m *Mri2) Description() string {
+	return "MRI sensor aggregator V2"
+}
+
+func (m *Mri2) Init() error {
+	return nil
+}
+
+type byDate []string
+
+func (s byDate) Len() int {
+	return len(s)
+}
+
+func (s byDate) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func parseDate(date, t string, loc *time.Location) time.Time {
+	day := parseInt(date[0:2])
+	month := time.Month(parseInt(date[2:4]))
+	year := parseInt(date[4:6]) + 2000
+
+	hour := parseInt(t[0:2])
+	minute := parseInt(t[2:4])
+
+	return time.Date(year, month, day, hour, minute, 0, 0, loc)
+}
+
+func (s byDate) Less(i, j int) bool {
+	// is s[i] less than s[j]
+	lDay, lMonth, lYear := s[i][3:5], s[i][5:7], s[i][7:9]
+	rDay, rMonth, rYear := s[j][3:5], s[j][5:7], s[j][7:9]
+
+	l := lYear + lMonth + lDay
+	r := rYear + rMonth + rDay
+
+	return strings.Compare(l, r) == 1
+}
+
+func filterDates(entries []string) (out []string) {
+	for _, s := range entries {
+		if strings.HasPrefix(s, "day") {
+			out = append(out, s)
+		}
+	}
+	return
+}
+
+func parseInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func parseFloat(s string) float64 {
+	f, _ := strconv.ParseFloat(s, 64)
+	return f
+}
+
+type row struct {
+	fields map[string]interface{}
+	time   time.Time
+}
+
+func parseData(data io.Reader) (out []row) {
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+
+		out = append(out,
+			row{
+				fields: map[string]interface{}{
+					"HeLvl":       parseFloat(fields[3]),
+					"H20_Flow":    parseFloat(fields[4]),
+					"H20_Temp":    parseFloat(fields[6]),
+					"Shield":      parseFloat(fields[8]),
+					"ReconRuO":    parseFloat(fields[9]),
+					"ReconSi410":  parseFloat(fields[10]),
+					"ColdheadRuO": parseFloat(fields[13]),
+					"HePress":     parseFloat(fields[14]),
+					"HDC":         parseInt(fields[26]),
+				},
+				time: parseDate(fields[0], fields[1], time.Now().Location()),
+			})
+	}
+
+	fmt.Printf("Gathered %d parsed fields\n", len(out))
+	return
+}
+
+func (m *Mri2) gatherStats(ctx context.Context, url *url.URL) ([]row, error) {
+	fmt.Printf("Connecting to FTP server: %v\n", url.Redacted())
+
+	username := m.DefaultUsername
+	password := m.DefaultPassword
+
+	if userInfo := url.User; userInfo != nil {
+		username = userInfo.Username()
+		if userInfoPassword, ok := userInfo.Password(); ok {
+			password = userInfoPassword
+		}
+	}
+
+	ftpClient, err := ftp.Dial(url.Host, ftp.DialWithContext(ctx))
+	if err != nil {
+		fmt.Printf("Failed to connect to FTP server: %v\n", err)
+		return nil, err
+	}
+
+	if err := ftpClient.Login(username, password); err != nil {
+		fmt.Printf("Failed to login to FTP server, is the username:password correct?: %v\n", err)
+		return nil, err
+	}
+
+	defer ftpClient.Quit()
+
+	entries, err := ftpClient.NameList("/CFDISK/mindata")
+	if err != nil {
+		fmt.Printf("Failed to list /CFDISK/mindata: %v\n", err)
+		return nil, err
+	}
+
+	entries = filterDates(entries)
+	sort.Sort(byDate(entries))
+
+	if len(entries) == 0 {
+		fmt.Println("Directory is empty")
+		return nil, errors.New("no entries found")
+	}
+
+	// retrieve top file
+	latestDat := entries[0]
+
+	fmt.Printf("Parsing file: %v\n", latestDat)
+
+	rsp, err := ftpClient.Retr(fmt.Sprintf("/CFDISK/mindata/%s", latestDat))
+	if err != nil {
+		return nil, err
+	}
+
+	defer rsp.Close()
+
+	return parseData(rsp), nil
+}
+
+func (m *Mri2) Gather(acc telegraf.Accumulator) error {
+
+	ctx := context.Background()
+
+	errgrp, ctx := errgroup.WithContext(ctx)
+
+	for _, machine := range m.Machines {
+		url, err := url.Parse(machine.URL)
+		if err != nil {
+			fmt.Printf("invalid url")
+			return err
+		}
+
+		errgrp.Go(func() error {
+			rows, err := m.gatherStats(ctx, url)
+			if err != nil {
+				return err
+			}
+			tags := map[string]string{
+				"collectorName": m.CollectorName,
+				"name":          machine.Name,
+			}
+			for _, row := range rows {
+				acc.AddFields("mridata", row.fields, tags)
+			}
+
+			return nil
+		})
+	}
+
+	if err := errgrp.Wait(); err != nil {
+		return err
+	}
+
+	// wait for writing to finish
+	return nil
+}
+
+func init() {
+	var name = ""
+	name, _ = os.Hostname()
+	inputs.Add("mri", func() telegraf.Input {
+		return &Mri2{
+			CollectorName: name,
+		}
+	})
+}
---
+++ plugins/inputs/mri2/mri2_test.go
@@ -0,0 +1,126 @@
+package mri
+
+import (
+	"net/url"
+	"os"
+	"sort"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/influxdata/toml"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfiguration(t *testing.T) {
+	var mri Mri2
+
+	sampleConfig := `
+CollectorName = "Collector1"
+Machines = [
+	{ Name = "Server1", URL = "ftp://server1.com" }, 
+	{ Name = "Server2", URL = "ftp://overrideUser:overridePassword@server2.com" }
+]
+DefaultUsername = "user"
+DefaultPassword = "daki123"
+`
+
+	sampleConfig = strings.ReplaceAll(sampleConfig, "#", "")
+
+	t.Log(sampleConfig)
+	assert.NoError(t, toml.Unmarshal([]byte(sampleConfig), &mri))
+	assert.Len(t, mri.Machines, 2)
+	url, err := url.Parse(mri.Machines[1].URL)
+	assert.NoError(t, err)
+	assert.Equal(t, url.User.String(), "overrideUser:overridePassword")
+}
+
+func TestFilterDates(t *testing.T) {
+	entries := []string{
+		"day290519.dat",
+		"day300519.dat",
+		"day310519.dat",
+		"lastsaved.dat",
+		"lastsent.dat",
+	}
+	expected := []string{
+		"day290519.dat",
+		"day300519.dat",
+		"day310519.dat",
+	}
+
+	actual := filterDates(entries)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestSortByDate(t *testing.T) {
+	entries := []string{
+		"day290519.dat",
+		"day300519.dat",
+		"day010120.dat",
+		"day310519.dat",
+	}
+	expected := []string{
+		"day010120.dat",
+		"day310519.dat",
+		"day300519.dat",
+		"day290519.dat",
+	}
+
+	sort.Sort(byDate(entries))
+	assert.Equal(t, expected, entries)
+}
+
+func TestParseData(t *testing.T) {
+	f, _ := os.Open("testdata/day310519.dat")
+
+	out := parseData(f)
+
+	expected := []row{
+		row{
+			fields: map[string]interface{}{
+				"ColdheadRuO": 4.24,
+				"H20_Flow":    12.878,
+				"H20_Temp":    30.381,
+				"HDC":         21,
+				"HeLvl":       68.344,
+				"HePress":     1.083,
+				"ReconRuO":    4.215,
+				"ReconSi410":  3.504,
+				"Shield":      39.222,
+			},
+			time: parseDate("310519", "2358", time.Now().Location()),
+		},
+		row{
+			fields: map[string]interface{}{
+				"ColdheadRuO": 4.19,
+				"H20_Flow":    12.98,
+				"H20_Temp":    30.545,
+				"HDC":         21,
+				"HeLvl":       68.344,
+				"HePress":     1.079,
+				"ReconSi410":  3.504,
+				"ReconRuO":    4.215,
+				"Shield":      39.222,
+			},
+			time: parseDate("310519", "2359", time.Now().Location()),
+		},
+	}
+
+	assert.Equal(t, expected, out)
+}
+
+func TestParseDate(t *testing.T) {
+	dateStr := "230619"
+	timeStr := "1026"
+
+	location, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		panic(err)
+	}
+
+	actual := parseDate(dateStr, timeStr, location)
+	expected := time.Unix(1561310760, 0)
+	assert.Equal(t, expected.UTC(), actual.UTC())
+}
